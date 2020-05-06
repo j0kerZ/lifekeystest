d1cd5a94a7ba07bc3968c8ac7446848d338f3109
j0kerZ <joker.wartune1527@gmail.com>
2020-05-06 03:43:11 +0200

    Updates

---
+++ precache-manifest.d462a1bb02884eba2340ef0a028fbb31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abee89c6caa2d18cf68b14685b8aa48b",
+    "url": "/lifekeystest/index.html"
+  },
+  {
+    "revision": "c3f8454ff2ae2c3ef7d2",
+    "url": "/lifekeystest/static/css/main.76b6a31a.chunk.css"
+  },
+  {
+    "revision": "74f49c362c07810db95b",
+    "url": "/lifekeystest/static/js/2.f17e5d48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lifekeystest/static/js/2.f17e5d48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3f8454ff2ae2c3ef7d2",
+    "url": "/lifekeystest/static/js/main.fb38aae6.chunk.js"
+  },
+  {
+    "revision": "84e814f37f774e9fc7ae",
+    "url": "/lifekeystest/static/js/runtime-main.0ced43c7.js"
+  }
+]);
